a3a0ceb8034b407550a4d582ee1edd27119b9084
Sparsh Koyambreth <skoyamb@ncsu.edu>
2024-11-11 01:41:50 -0500

    fixed authentication style

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -9,15 +9,15 @@ export default function Page() {
     <div className="h-full lg:flex flex-col items-center justify-center px-4">
       <div className="text-center space-y-4 pt-16">
         <h1 className="font-bold text-3xl text-[#2E2A47]">
-          Welcome Back!
+          Create an Account
         </h1>
         <p className="text-base text-[#7E8CA0]">
-          Log in or Create Account to get back to your dashboard!
+          Enter your details below to start your finance journey!
         </p>
       </div>
       <div className="flex items-center justify-center mt-8">
         <ClerkLoaded>
-          <SignUp path="/sign-up" />;
+          <SignUp path="/sign-up" />
         </ClerkLoaded>
         <ClerkLoading>
           <Loader2 className="animate-spin text-muted-foreground"/>
